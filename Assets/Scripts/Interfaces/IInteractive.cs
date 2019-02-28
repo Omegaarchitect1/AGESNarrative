ff3d41197722be5eb90a0417390c67b787ec7442
Mitchell Quigg <Mitchell.Quigg@iam.local>
2019-02-28 13:58:23 -0600

    started video 2

---
+++ Assets/Scripts/Interfaces/IInteractive.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Interface for when the player interacts with objects using the interact button
+/// </summary>
+public interface IInteractive
+{
+    void InteractWith();
+}
+
