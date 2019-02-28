03a0298a326d562e295c0d050347af1aec87893a
Mitchell Quigg <Mitchell.Quigg@iam.local>
2019-02-28 12:33:46 -0600

    started playercontrols script

---
+++ Assets/Scripts/DetectedLookAtInterctor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectedLookAtInterctor : MonoBehaviour
+{
+    //
+    [Tooltip("Starting point the raycast used to detect interactives.")]
+    [SerializeField]
+    private Transform raycastOrigin;
+
+    [Tooltip("How far from the raycast origin we'll search for interactive elements")]
+    [SerializeField]
+    private float maxRange = 5.0f;
+
+    private Vector3 raycastDirection;
+
+
+    private void FixedUpdate()
+    {
+        Physics.Raycast(raycastOrigin.position, raycastDirection, maxRange);
+    }
+}
