c90df6cf76fc18864461ef048e5f99a8e0bf977b
Mitchell Quigg <Mitchell.Quigg@iam.local>
2019-03-07 12:48:34 -0600

    Started events scripts

---
+++ Assets/Scripts/LookedAtInteractiveDisplayText.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LookedAtInteractiveDisplayText : MonoBehaviour
+{
+	private IInteractive LookedAtInteractive;
+	private Text
+	
+}
