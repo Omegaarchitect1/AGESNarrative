bf03923f9ca8e98d4aaef62b0eb9c04c4b39ae09
Mitchell Quigg <Mitchell.Quigg@iam.local>
2019-04-04 13:53:45 -0500

    started 2nd video, began scripts on player inventory

---
+++ Assets/Scripts/InventoryObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObject : InteractiveObject
+{
+    // TODO: add long desctiption field
+    // TODO: add item icon
+
+    public override void InteractWith()
+    {
+        base.InteractWith();
+        PlayerInventory p = new PlayerInventory();
+        p.InventoryObjects.Add(this);
+
+    }
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerInventory 
+{
+    public List<InventoryObject> InventoryObjects { get; } = new List<InventoryObject>();
+
+   
+}
