b2c9c451ceb2eee0f282cbe6cd9d98eb525778f2
Mitchell Quigg <Mitchell.Quigg@iam.local>
2019-05-02 12:02:19 -0500

    added a flashlight!

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashlight;
+    public bool activatelashlight = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activatelashlight = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown ("Flashlight"))
+        {
+            if (activatelashlight == true)
+            {
+                flashlight.enabled = false;
+                activatelashlight = false;
+            }
+            else if (activatelashlight == false)
+            {
+                flashlight.enabled = true;
+                activatelashlight = true; 
+            }
+        }
+    }
+}
